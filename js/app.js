0c1f7a9b6f3c27f15167445517575f792ef3d5d5
carmen <carmelaespinola@gmail.com>
2026-02-11 07:33:07 +0100

    cambio en el orden de la info

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var check=document.querySelector(".check");
+/* var check=document.querySelector(".check");
 check.addEventListener('click',idioma);
 function idioma(){
     let id=check.checked;
@@ -27,4 +27,36 @@ window.onscroll = function(){
         buttonUp.style.transform ="scale(0)";
     }
 
+} */
+// selector idioma
+const check = document.querySelector(".check");
+check.addEventListener("click", idioma);
+
+function idioma() {
+  let id = check.checked;
+  if (id === true) {
+    location.href = "es/index.html";
+  } else {
+    location.href = "../index.html";
+  }
 }
+
+// scroll up
+const buttonUp = document.getElementById("button-up");
+
+buttonUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+window.addEventListener("scroll", () => {
+  let scroll = document.documentElement.scrollTop;
+
+  if (scroll > 500) {
+    buttonUp.style.transform = "scale(1)";
+  } else {
+    buttonUp.style.transform = "scale(0)";
+  }
+});
